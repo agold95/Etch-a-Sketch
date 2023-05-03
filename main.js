f9323bff19f5b52fcacb3bf336d2d6c2bf745288
Adam Goldberg <112718049+agold95@user.noreply.github.com>
2023-05-03 14:14:09 -0400

    added functionality

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
-const container = document.querySelector('.container');
-const gridSize = 16;
+const gridContainer = document.querySelector('.grid-container');
+const gridSize = document.querySelector('input');
+let squareSize = 8;
+let gridValue = document.querySelector('.grid-size')
+let apply = document.querySelector('.apply')
+let resetButton = document.querySelector('.reset')
 
-createBlank(gridSize);
-
-function createSquare () {
+function createSquare (size) {
     const square = document.createElement('div');
     square.classList.add('square');
-    square.style.width = '16px';
-    square.style.height = '16px';
+    square.style.width = `${size}px`;
+    square.style.height = `${size}px`;
     return square;
 }
 
 function createBlank(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
-            container.appendChild(createSquare());
+            gridContainer.appendChild(createSquare(gridContainer.clientWidth / gridSize));
         }
     }
 }
+
+createBlank(squareSize);
+
+gridContainer.addEventListener('mouseover', (e) => {
+    if (e.target.matches('.square')) {
+        e.target.classList.add('active');
+    }
+});
+
+gridSize.addEventListener('input', (e) => {
+    squareSize = e.target.value;
+    gridValue.textContent = `${squareSize}X${squareSize}`;
+});
+
+function reset () {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    createBlank(squareSize);
+}
+
+apply.addEventListener('click', () => {
+    reset();
+});
+
+resetButton.addEventListener('click', reset);
